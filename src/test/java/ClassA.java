9922f29dbea59211015007ff7f658cb5923f0f9b
mshaveta <shaveta.malhotra47@gmail.com>
2022-01-29 19:17:10 +0530

    CommitA

---
+++ src/test/java/ClassA.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class ClassA {
+    @Test
+    public void m1(){
+        System.out.println("m1");
+    }
+
+    @Test
+    public void m2(){
+        System.out.println("m2");
+    }
+}
